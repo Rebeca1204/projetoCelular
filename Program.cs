e3e0328be27e92e0f53e4f990701f73e1248c2f1
Rebeca Furtado <66632482+Rebeca1204@users.noreply.github.com>
2023-12-27 12:15:31 +0000

    commit teste classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// NOKIA
+Console.WriteLine("NOKIA");
+Smartphone nokia = new Nokia(numero: "992235332", modelo: "modelo X", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+// IPHONE   
+Console.WriteLine("IPHONE");
+Smartphone iphone = new Iphone("983410042", "modelo 15", "222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
